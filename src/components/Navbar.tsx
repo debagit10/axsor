d841a64e706cb33bbde473dd8d7fd0653df8112d
Josiah Elias <josiahelias11@gmail.com>
2025-11-17 20:54:04 +0100

    fix: adjust padding in Navbar component for improved layout consistency

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   console.log(route);
 
   return (
-    <div className="fixed top-0 left-0 w-full z-50 px-[8%] bg-[#FFFFFF0D]/24 backdrop-blur-md">
+    <div className="fixed top-0 left-0 w-full z-50  bg-[#FFFFFF0D]/24 backdrop-blur-md">
       <div className="px-[8%] pt-6 text-[#D3D2CE]">
         <div className="flex  items-center justify-between ">
           <img
