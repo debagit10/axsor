5b691b17d1a689122be25c455c0018e4452d37ba
Josiah Elias <josiahelias11@gmail.com>
2025-11-21 19:18:36 +0100

    fix: refactor Blog component to fetch and display blog data dynamically; replace static news items with fetched data

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,23 +1,66 @@
 import { Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+
+interface Blogs {
+  id: number;
+  acf: {
+    content: string;
+    read_time: string;
+    url: string;
+    image: { url: string; id: string };
+  };
+}
 
 const Blog = () => {
-  const newsItems = [
-    {
-      title: "Axsor Energy Expands Distribution Network Across Ghana...",
-      readTime: "3 MINS READ",
-      image: "/images/news1.jpg", // replace with actual image path
-    },
-    {
-      title: "Axsor Energy Launches New LPG Facility in Takoradi...",
-      readTime: "4 MINS READ",
-      image: "/images/news2.jpg",
-    },
-    {
-      title: "Empowering Local Communities Through Sustainable Energy...",
-      readTime: "5 MINS READ",
-      image: "/images/news3.jpg",
-    },
-  ];
+  // const newsItems = [
+  //   {
+  //     title: "Axsor Energy Expands Distribution Network Across Ghana...",
+  //     rea: "3 MINS READ",
+  //     image: "/images/news1.jpg", // replace with actual image path
+  //   },
+  //   {
+  //     title: "Axsor Energy Launches New LPG Facility in Takoradi...",
+  //     rea: "4 MINS READ",
+  //     image: "/images/news2.jpg",
+  //   },
+  //   {
+  //     title: "Empowering Local Communities Through Sustainable Energy...",
+  //     rea: "5 MINS READ",
+  //     image: "/images/news3.jpg",
+  //   },
+  // ];
+
+  const [blogs, setBlogs] = useState<Blogs[]>([]);
+
+  useEffect(() => {
+    fetch(
+      "https://axsor-f6fd39.ingress-alpha.ewp.live/wp-json/wp/v2/blogs?order=asc&orderby=date"
+    )
+      .then((res) => res.json())
+      .then(async (data) => {
+        const blogsWithImages = await Promise.all(
+          data.map(async (blog: any) => {
+            if (blog.acf?.image && typeof blog.acf.image === "number") {
+              try {
+                const imgRes = await fetch(
+                  `https://axsor-f6fd39.ingress-alpha.ewp.live/wp-json/wp/v2/media/${blog.acf.image}`
+                );
+                const imgData = await imgRes.json();
+                blog.acf.image = {
+                  id: blog.acf.image,
+                  url: imgData.source_url,
+                  alt: imgData.alt_text || blog.acf.name,
+                };
+              } catch (error) {
+                console.error("Error fetching image:", error);
+              }
+            }
+            return blog;
+          })
+        );
+        setBlogs(blogsWithImages);
+      });
+  }, []);
 
   return (
     <div className="relative w-full bg-[url('/bg_home.jpg')] bg-cover  bg-no-repeat">
@@ -50,14 +93,14 @@ const Blog = () => {
           </div>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-8 justify-center">
-            {newsItems.map((item, index) => (
+            {blogs.map((item, index) => (
               <div
+                onClick={() => window.open(`${item.acf.url}`, "_blank")}
                 key={index}
                 className="relative md:w-full w-[333px] h-[494px] md:h-[454px] bg-[#00000005] rounded-[20px] overflow-hidden mx-auto"
               >
                 <img
-                  src={item.image}
-                  alt={item.title}
+                  src={item.acf.image.url}
                   className="w-full h-full object-cover"
                 />
 
@@ -69,7 +112,7 @@ const Blog = () => {
                       color: "#787878",
                     }}
                   >
-                    {item.title}
+                    {item.acf.content.split(" ").slice(0, 10).join(" ") + "..."}
                   </Typography>
 
                   <Typography
@@ -79,7 +122,7 @@ const Blog = () => {
                       color: "#000000",
                     }}
                   >
-                    {item.readTime}
+                    {item.acf.read_time}
                   </Typography>
                 </div>
               </div>
